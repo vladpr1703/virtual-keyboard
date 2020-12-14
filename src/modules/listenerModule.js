64a3edb9f0a4d1e51de1bfac778ceb1716c4efe0
vladpr1703 <vlad.prusakov1703@gmail.com>
2020-12-14 10:26:22 +0300

    changes

--- src/modules/listenerModule.js
+++ src/modules/listenerModule.js
@@ -2,40 +2,54 @@ export default function addListener() {
   document.addEventListener('click', (event) => {
     const textArea = document.querySelector('#text');
     const condition = textArea.value.length === textArea.selectionEnd || textArea.selectionEnd === 0;
-    const cursor = textArea.selectionEnd;
+    let cursor = textArea.selectionEnd;
     switch (event.target.className) {
       case 'key backspace':
+        textArea.focus();
         if (condition) {
           textArea.value = textArea.value.slice(0, textArea.value.length - 1);
         }
-        else { textArea.value = textArea.value.slice(0, (cursor - 1))  
+        else { 
+          textArea.value = textArea.value.slice(0, (cursor - 1))  
           + textArea.value.slice(cursor, textArea.value.length);
+          textArea.selectionStart = textArea.selectionEnd = textArea.value.slice(0, (cursor - 1)).length;
         }
         break;
       case 'key enter':
+        textArea.focus();
         textArea.value = textArea.value.slice(0, cursor) + '\n' + textArea.value.slice(cursor, textArea.value.length);
+        textArea.selectionStart = textArea.selectionEnd = textArea.value.slice(0, (cursor + 1)).length;
         break;
       case 'key space':
+        textArea.focus();
         if (condition) {
           textArea.value = textArea.value.slice(0, textArea.value.length) + ' ';
         }
-        else { textArea.value = textArea.value.slice(0, (cursor)) + ' ' +  textArea.value.slice(cursor, textArea.value.length);
+        else { 
+          textArea.value = textArea.value.slice(0, (cursor)) + ' ' 
+          +  textArea.value.slice(cursor, textArea.value.length);
+          textArea.selectionStart = textArea.selectionEnd = textArea.value.slice(0, (cursor + 1)).length;
         }
         break;
       case 'key': 
+      textArea.focus();
         if (condition) {
           textArea.value = textArea.value.slice(0, textArea.value.length) + event.target.textContent;
         }
-        else { textArea.value = textArea.value.slice(0, (cursor))  
+        else { 
+          textArea.value = textArea.value.slice(0, (cursor))  
           + event.target.textContent + textArea.value.slice(cursor, textArea.value.length);
+          textArea.selectionStart = textArea.selectionEnd = textArea.value.slice(0, (cursor+1)).length;
       }
         break;
       case 'key key_caps':
+        textArea.focus();
         if (condition) {
           textArea.value = textArea.value.slice(0, textArea.value.length) + event.target.textContent.toUpperCase();
         }
-        else { textArea.value = textArea.value.slice(0, (condition))  
-          + event.target.textContent.toUpperCase() +  textArea.value.slice(condition, textArea.value.length);
+        else { textArea.value = textArea.value.slice(0, (cursor))  
+          + event.target.textContent.toUpperCase() +  textArea.value.slice(cursor, textArea.value.length);
+          textArea.selectionStart = textArea.selectionEnd = textArea.value.slice(0, (cursor+1)).length;
         }
         break;
       case 'key capslock': 
